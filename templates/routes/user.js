7a6c4eedd1952c73d2927833c4ad510dc7964d48
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-04-23 20:08:14 +0300

    Update user.js

--- templates/routes/user.js
+++ templates/routes/user.js
@@ -1,14 +1,7 @@
 module.exports = `const router = require("express").Router()
-const {
-  getUsers,
-  getUserById,
-  updateUser,
-  deleteUser,
-  getUserProfile,
-  registerUser,
-  authUser,
-} = require("../controllers/user")
+const { getUsers, getUserById, updateUser, deleteUser, getUserProfile, registerUser, authUser } = require("../controllers/user")
 const { isAuth, isAdmin } = require("../middleware/auth")
+const { createAccountLimiter } = require('../middleware/rateLimiter')
 
 router.route("/").get(isAuth, isAdmin, getUsers)
 router.route("/profile").get(isAuth, getUserProfile)
@@ -17,7 +10,7 @@ router
   .get(isAuth, isAdmin, getUserById)
   .put(isAuth, isAdmin, updateUser)
   .delete(isAuth, isAdmin, deleteUser)
-router.route("/register").post(registerUser)
+router.route("/register").post(createAccountLimiter, registerUser)
 router.route("/login").post(authUser)
 
 module.exports = router
